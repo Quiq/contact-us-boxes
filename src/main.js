d1ed49371001eba898e278f0815c398409d41f5e
Nate Norberg <nate.norberg@quiq.com>
2020-05-12 14:00:49 -0600

    Added reconfigure

--- src/main.js
+++ src/main.js
@@ -3,14 +3,42 @@ import validateConfiguration from './validateConfiguration'
 import render from './render'
 
 var config = {}
+var rendered = false
+
+function updateConfig(oldConfig, newConfig) {
+  let channels = Object.assign({}, oldConfig.channels, newConfig.channels)
+
+  let styles
+  if (oldConfig.styles || newConfig.styles) {
+    styles = Object.assign({}, oldConfig.styles, newConfig.styles)
+  }
+
+  return Object.assign({}, oldConfig, newConfig, {channels, styles})
+}
 
 const QuiqContactUs = {
   configure: function (configuration) {
     validateConfiguration(configuration)
     config = configuration
   },
+  reconfigure(newConfig) {
+    const mergedConfig = updateConfig(config, newConfig)
+    console.log('new config is', mergedConfig)
+
+    this.configure(mergedConfig)
+    if (rendered) {
+      this.unrender()
+      this.render()
+    }
+  },
   render() {
+    rendered = true
     render({config})
+  },
+  unrender() {
+    document.querySelector('#QuiqContactUsButtons').remove()
+    document.querySelector('#QuiqContactUsButton').remove()
+    document.querySelector('.QuiqContactUs-modalContainer').remove()
   },
   close: function () {
     var container = document.querySelector('#QuiqContactUsButtons')

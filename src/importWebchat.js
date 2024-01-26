c5a1ba7027ff387ac401265108b210691781d6cb
Nate Norberg <nate.norberg@quiq.com>
2024-01-26 10:33:33 -0700

    Things are building, fonts are switching, chat is rendering an extra button

--- src/importWebchat.js
+++ src/importWebchat.js
@@ -1,7 +1,7 @@
 export default function importWebchat({tenant, options}) {
   return new Promise((resolve) => {
     const script = document.createElement('script')
-    script.setAttribute('src', `https://${tenant}.quiq-api.com/app/webchat/index.js`)
+    script.setAttribute('src', `https://${tenant}.quiq-api.com/app/chat-ui/index.js`)
     script.setAttribute('charset', 'UTF-8')
     script.onload = () => {
       resolve(Quiq(options))

68dd7dbdef747efeff55ce8443d73dc862650504
Nate Norberg <nate.norberg@quiq.com>
2025-03-29 15:24:17 -0600

    Added failing test for chat 2.0 import

--- src/__tests__/webchatImport.test.js
+++ src/__tests__/webchatImport.test.js
@@ -3,10 +3,12 @@ import 'regenerator-runtime/runtime'
 import {getByText, fireEvent, waitFor} from '@testing-library/dom'
 import {TestScheduler} from 'jest'
 
-test('legacy webchat import', async () => {
+beforeEach(() => {
   window.Quiq = jest.fn()
   jest.spyOn(document.body, 'appendChild')
+})
 
+test('legacy webchat import', async () => {
   const testConfig = {
     channels: {
       webchat: {
@@ -22,6 +24,27 @@ test('legacy webchat import', async () => {
     expect(document.body.appendChild).toHaveBeenCalledWith(
       expect.objectContaining({
         src: 'https://nate.quiq-api.com/app/webchat/index.js',
+      }),
+    ),
+  )
+})
+
+test('chat 2.0 import', async () => {
+  const testConfig = {
+    channels: {
+      webchat: {
+        tenant: 'nate',
+        options: {pageConfigurationId: 'default'},
+      },
+    },
+    order: ['webchat'],
+  }
+  render({config: testConfig})
+
+  await waitFor(() =>
+    expect(document.body.appendChild).toHaveBeenCalledWith(
+      expect.objectContaining({
+        src: 'https://nate.quiq-api.com/app/chat-ui/index.js',
       }),
     ),
   )
